49425a41d8c855f764145400426be99305f6e7f1
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-13 16:10:36 +0530

    Added User Model

---
+++ reddit/src/main/java/com/manish/reddit/model/User.java
@@ -0,0 +1,102 @@
+package com.manish.reddit.model;
+
+import java.time.Instant;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+public class User {
+	
+	@Id
+	private Long userId;
+	
+	
+	@NotBlank(message = "Username is Required")
+	private String userName;
+	
+	
+	@NotBlank(message = "Password is Required")
+	private String password;
+	
+	
+	@Email
+	@NotEmpty(message = "Email is required")
+	private String email;
+	
+	
+	private Instant created;
+	
+	private Boolean enabled;
+
+	public User(Long userId, @NotBlank(message = "Username is Required") String userName,
+			@NotBlank(message = "Password is Required") String password,
+			@Email @NotEmpty(message = "Email is required") String email, Instant created, Boolean enabled) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.created = created;
+		this.enabled = enabled;
+	}
+
+	public User() {
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Instant getCreated() {
+		return created;
+	}
+
+	public void setCreated(Instant created) {
+		this.created = created;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+	
+	
+}
