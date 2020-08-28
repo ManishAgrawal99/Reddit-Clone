c6e568188b7f4c1d280654292b676c76f8bd9cf5
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-08-28 20:47:32 +0530

    JWT validation while Login added

--- reddit/src/main/java/com/manish/reddit/security/JwtProvider.java
+++ reddit/src/main/java/com/manish/reddit/security/JwtProvider.java
@@ -6,6 +6,7 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
@@ -17,7 +18,10 @@ import org.springframework.stereotype.Service;
 import com.manish.reddit.exception.SpringRedditException;
 import org.springframework.security.core.userdetails.User;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+
+import static io.jsonwebtoken.Jwts.parser;
 
 @Service
 public class JwtProvider {
@@ -53,5 +57,29 @@ public class JwtProvider {
 		} catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
 			throw new SpringRedditException("Exception ocured while retrieving public key from keystore");
 		}
+	}
+	
+	
+	public boolean validateToken(String jwt) {
+		parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+		return true;
+	}
+
+
+	private PublicKey getPublicKey() {
+		try {
+			return keyStore.getCertificate("springblog").getPublicKey();
+		} catch (KeyStoreException e) {
+			throw new SpringRedditException("Exception occured while retrieving public key from key store");
+		}
+	}
+	
+	public String getEmailFromJwt(String token) {
+		
+		Claims claims = parser().setSigningKey(getPublicKey())
+								.parseClaimsJws(token)
+								.getBody();
+		
+		return claims.getSubject();
 	}
 }
